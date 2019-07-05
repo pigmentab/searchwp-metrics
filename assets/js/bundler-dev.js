f973559611af1d62f0bd7e57d4543398f35bdaae
Pigment <fredrik@fredrikbrolin.se>
2019-07-05 10:03:39 +0200

    add plugin files

---
+++ assets/js/bundler-dev.js
@@ -0,0 +1,17 @@
+const Bundler = require('parcel-bundler');
+const Path = require('path');
+
+const file = Path.join(__dirname, './src/main.js');
+
+const devOptions = {
+	outDir: Path.join(__dirname, './dist'),
+	outFile: 'bundle.js',
+	watch: true,
+	cache: false,
+	minify: false,
+	hmr: false
+};
+
+const devBundler = new Bundler(file, devOptions);
+
+devBundler.bundle();
